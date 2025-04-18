29dd221a332708324887ae571f5249ebdd000b5f
bedlam343 <jagik22@gmail.com>
2025-04-18 15:22:07 -0700

    change "Person" to "User"

--- queries.py
+++ queries.py
@@ -5,7 +5,7 @@ def get_following(tx, name):
     """
     Handles the query and runs the transaction to return the nodes a user is following.
     """
-    query = """MATCH (p:Person {name: $name})-[:FOLLOWS]->(f:Person) 
+    query = """MATCH (p:User {name: $name})-[:FOLLOWS]->(f:User) 
         RETURN f"""
     nodes = tx.run(query, name=name)
     return [node["f"]["name"] for node in nodes]
@@ -19,15 +19,15 @@ def execute_get_following_():
     with driver.session() as session:
         followed = session.execute_read(get_following, name="Rachel Lally") # substitute for the user's name later
         print('\033[1m'"\033[4m" + "Your Following List:" + '\033[0m')
-        for person in followed:
-            print(person)
+        for user in followed:
+            print(user)
 
 # # View Friends/Connections - A user can see a list of people who follow them.
 def get_followers(tx, name):
     """
     Handles the query and runs the transaction to return the nodes that follow a user.
     """
-    query = """MATCH (p:Person {name: $name})<-[:FOLLOWS]-(f:Person) 
+    query = """MATCH (p:User {name: $name})<-[:FOLLOWS]-(f:User) 
         RETURN f"""
     nodes = tx.run(query, name=name)
     return [node["f"]["name"] for node in nodes]
@@ -41,8 +41,12 @@ def execute_get_followers():
     with driver.session() as session:
         followers = session.execute_read(get_followers, name="Ganesh Bisht") # substitute for the user's name later
         print('\033[1m'"\033[4m" + "Your Followers:" + '\033[0m')
-        for person in followers:
-            print(person)
+        for user in followers:
+            print(user)
 
 # # Mutual Connections - A user can see mutual friends (users followed by both parties).
 # # def get_mutuals(friend):
+
+def create_user(name, username, email, password):
+    query = """MATCH (p:User {name: $name})<-[:FOLLOWS]-(f:User) 
+        RETURN f"""
